41dffb2f53a2c6cf08a8716e3b7538defc7ab12a
Keith Null <keith1126@126.com>
2020-05-11 13:50:44 +0800

    Remove html responses in backend

--- server.py
+++ server.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-
 import entry
 
-html_file = "./page.html"
-
-
-def get_html_page(file_path: str) -> str:
-    with open(file_path, mode="r", encoding="utf8") as f:
-        return f.read()
-
-html_content = get_html_page(html_file)
 
 app = FastAPI()
 origins = [
@@ -25,10 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/", response_class=HTMLResponse)
-def read_root():
-    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/search/{author_name}")

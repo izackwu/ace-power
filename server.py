3f82d3c0175e71e32cfe84f5d54083fcaf01bba7
Keith Null <keith1126@126.com>
2020-05-11 13:46:50 +0800

    Config backend to allow CORS for dev

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 import entry
 
@@ -10,10 +11,20 @@ def get_html_page(file_path: str) -> str:
     with open(file_path, mode="r", encoding="utf8") as f:
         return f.read()
 
-
-app = FastAPI()
 html_content = get_html_page(html_file)
 
+app = FastAPI()
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", response_class=HTMLResponse)
 def read_root():
